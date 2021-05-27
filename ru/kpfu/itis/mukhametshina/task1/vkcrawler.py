4c43a26a86d1a07784dd2733ca528680461f0c7d
Alsu <alsu.mukhametshina57@gmail.com>
2021-05-27 15:36:02 +0300

    vk

--- ru/kpfu/itis/mukhametshina/task1/vkcrawler.py
+++ ru/kpfu/itis/mukhametshina/task1/vkcrawler.py
@@ -1,4 +1,16 @@
+import re
+
 import vk_api
+
+
+def deEmojify(text):
+    regrex_pattern = re.compile(pattern="["
+                                        u"\U0001F600-\U0001F64F"  # emoticons
+                                        u"\U0001F300-\U0001F5FF"  # symbols & pictographs
+                                        u"\U0001F680-\U0001F6FF"  # transport & map symbols
+                                        u"\U0001F1E0-\U0001F1FF"  # flags (iOS)
+                                        "]+", flags=re.UNICODE)
+    return regrex_pattern.sub(r'', text)
 
 
 def vk_craw(login, password):
@@ -16,6 +28,7 @@ def vk_craw(login, password):
     for i in range(200):
         words = wall['items'][i]['text'].split(' ')
         for word in words:
+            word = deEmojify(word)
             if answer.get(word):
                 answer[word] = answer[word] + 1
             else:
@@ -24,4 +37,3 @@ def vk_craw(login, password):
     answer = {k: v for k, v in sorted(answer.items(), key=lambda item: item[1])}
 
     return answer
-
